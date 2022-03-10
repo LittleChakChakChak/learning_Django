dec4b8a895f45ad069e7f9f2c45f63fb33cbaff6
littleChakChakChak <kovtundmitriy11@gmail.com>
2022-03-11 00:42:28 +0300

    Продолжение изучения django

--- taskmanager/main/views.py
+++ taskmanager/main/views.py
@@ -1,12 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import Task
+from .forms import TaskForm
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'main/index.html')
+    tasks = Task.objects.all()
+    return render(request, 'main/index.html', {'title': 'Главная страница сайта', 'tasks': tasks})
 
 
 def about(request):
     return render(request, 'main/about.html')
+
+
+def create(request):
+    error = ''
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            error = 'Форма была не верной!'
+
+    form = TaskForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'main/create.html', context)
